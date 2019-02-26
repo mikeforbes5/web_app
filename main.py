3c45b454e32b061453347e8cc6464a0aa7ddb2cc
mikeforbes5 <32751573+mikeforbes5@users.noreply.github.com>
2019-02-25 23:12:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def hello():
             if form.validate() == True:
                 name = request.form['name']
                 ytdl_format_options = {'format': 'bestaudio/best',
-                                       'outtmpl':'/home/mforbes5223/web_app/media/' + 'song'+ file_num + '.mp3',
+                                       'outtmpl':'~/web_app/media/' + 'song'+ file_num + '.mp3',
                                        'quiet': True}
                 with youtube_dl.YoutubeDL(ytdl_format_options) as ytdl:
                     ytdl.download([name])
@@ -32,7 +32,7 @@ def hello():
                     sc_title = sc_title.replace(":","").replace("<","").replace(">","").replace('"',"").replace("/","").replace("\\","").replace("|","").replace("?","").replace("*","")
                     sc_ext = info['ext']
                     sc_ext = sc_ext.replace(":","").replace("<","").replace(">","").replace('"',"").replace("/","").replace("\\","").replace("|","").replace("?","").replace("*","")
-                    return send_file(filename_or_fp='/home/mforbes5223/web_app/media/' + 'song'+ file_num + '.mp3',
+                    return send_file(filename_or_fp='~/web_app/media/' + 'song'+ file_num + '.mp3',
                                     mimetype='audio/mpeg', as_attachment=True,
                                     attachment_filename=sc_artist + " - " + sc_title + "." + sc_ext)
             else:
