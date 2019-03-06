71ee3a2778d752efaf5943fa2ac8350974b60f47
mikeforbes5 <32751573+mikeforbes5@users.noreply.github.com>
2019-03-05 20:52:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def hello():
                     sc_ext = info['ext']
                     sc_ext = sc_ext.replace(":","").replace("<","").replace(">","").replace('"',"").replace("/","").replace("\\","").replace("|","").replace("?","").replace("*","")
                     #with open('/tmp/' + 'song' + file_num + '.mp3', "rb") as file:
-                    return send_file(filename_or_fp=file,   
+                    return send_file(filename_or_fp='/tmp/' + 'song' + file_num + '.mp3',   
                                     mimetype='audio/mpeg', as_attachment=True,
                                     attachment_filename=sc_artist + sc_title + sc_ext)
                     #return send_file(filename_or_fp='/tmp/' + 'song'+ file_num + '.txt', mimetype='text/*', as_attachment=True, attachment_filename='song'+ file_num + '.txt')   
